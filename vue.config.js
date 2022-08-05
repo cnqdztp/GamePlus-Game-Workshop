38cfbd448efaca58200e3a7b983097a3b2db413d
气泡缓释胶囊 <japanztp@gmail.com>
2022-08-05 20:27:35 +0800

    proper icon and title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./src/index.html",
+//       filename: "index.html",
+//       title: "Some title",
+//       chunks: true
+//     })
+//   ]
+// })
+
+module.exports={
+  transpileDependencies : true,
+  chainWebpack: config =>{
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'Gameplus创作坊设计'
+          return args;
+        })
+  }
+}
